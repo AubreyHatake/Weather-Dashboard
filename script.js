1e5d00942e114490933d253ff75b737d80c20e6c
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-03-28 12:05:58 -0600

    adding style and adding more tags to html

---
+++ script.js
@@ -0,0 +1,17 @@
+// Variables will go here 
+
+
+
+
+
+
+
+
+
+
+// fetch Weather api function 
+// fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}')
+// .then(function (data) {
+//     console.log(data);
+//   });
+
