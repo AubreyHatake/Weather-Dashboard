ce8ab7fca7a534bc429d0cf34b84c664c1d23f89
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-03-28 12:32:18 -0600

    deleted some comments

--- script.js
+++ script.js
@@ -10,8 +10,4 @@
 
 
 // fetch Weather api function 
-// fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}')
-// .then(function (data) {
-//     console.log(data);
-//   });
 
