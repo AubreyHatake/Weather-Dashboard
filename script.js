f5ddf8c3f74a907d3a77e33d1bfd18ac46979e4b
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-04-01 16:26:33 -0600

    creating buttons and starting on api call

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var btn = document.querySelector('.btn');
 var weatherApi = "7383a191eb96b83d8fa7439817a5636c";
 var city;
 
-function x () {
-    console.log("You clicked a button");
+
+function getApi(city) {
+    var weatherURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + weatherApi;
+    fetch(weatherURL)
+    .then(function (city) {
+        console.log(city);
+    })
+    // console.log("You clicked a button");
 };
 
-btn.addEventListener("click", x);
+ btn.addEventListener("click", getApi);
 
 
 
