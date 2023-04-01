1d85fa511045870600424f3602aa2c43ea6cefd0
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-04-01 15:11:10 -0600

    creating variables

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Variables will go here 
+var btn = document.querySelector('.btn');
+var weatherApi = "7383a191eb96b83d8fa7439817a5636c";
+var city;
 
+function x () {
+    console.log("You clicked a button");
+};
 
-
-
+btn.addEventListener("click", x);
 
 
 
