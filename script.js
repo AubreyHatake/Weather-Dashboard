46340f3d60eeb0acabbd8e54f792d6c4a29813a0
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-04-03 12:07:39 -0600

    creating functions

--- script.js
+++ script.js
@@ -3,27 +3,50 @@ var btn = document.querySelector('.btn');
 var weatherApi = "7383a191eb96b83d8fa7439817a5636c";
 var currentCity= "";
 var lastCity = "";
+// currentCity= $('#search-city').val();
 
 
 // Function to get and display the current conditions on Open Weather Maps
-var getCurrentConditions = (event) => {
+var getCurrentConditions = (city) => {
     // Obtain city name from the search box
-    var city = $('#search-city').val();
-    currentCity= $('#search-city').val();
-    // Set the queryURL to fetch from API using weather search - added units=imperial to fix
-    var weatherURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&units=imperial" + "&APPID=" + weatherApi;
+    // Set the weatherURL to fetch from API using weather search
+    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + weatherApi;
     fetch(weatherURL)
     .then((response) => {
         console.log(response);
-        return response.json();
-    })
-    // .then((response) => {
-        // Save city to local storage
-        // $('#search-city')
+        return response.json();  
+    }) 
+    .then(data => {
+        console.log("CURR DAY: ", data);
+        getFiveDays(data.coord)
+    }) 
+    
+    
 };
-// })};
 
-btn.addEventListener("click", getCurrentConditions);
+function getFiveDays (coord) {
+    var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${weatherApi}`
+    fetch(fiveDayURL)
+    .then((response) => {
+        console.log(response);
+        return response.json();  
+    }) 
+    .then(data => {
+        
+        for(let i = 0; i < data.list.length; i+=8) {
+            console.log("FIVE DAYS: ", data.list[i]);
+        }
+    }) 
+    
+}
+
+
+btn.addEventListener("click", function () {
+    var city = $('#search-city').val()
+    getCurrentConditions(city)
+});
+
+
 
 
 
