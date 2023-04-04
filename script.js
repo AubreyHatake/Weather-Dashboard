c11bde4b603993c9cd440781bd3ea1975da76f8b
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-04-03 23:50:41 -0600

    commit

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var btn = document.querySelector('.button');
 var weatherApi = "7383a191eb96b83d8fa7439817a5636c";
 var currentCity= "";
 var lastCity = "";
-var ul = document.querySelector('#city-results');
-var div = document.querySelector('#current-weather');
+var results = document.querySelector('#city-results');
+var currentWeather = document.querySelector('#current-weather');
+var names = document.querySelector('.hide');
+var fiveDays = document.querySelector('#five-day-forecast');
 
 
 
@@ -15,31 +17,57 @@ var getCurrentConditions = (city) => {
     var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + weatherApi;
     fetch(weatherURL)
     .then((response) => {
+        // this if statement will  handle errors
+        if (response.status === 404) {
+            alert("hmm try typing that again..");
+            return;
+        };
         // set the city input in local storage
         localStorage.setItem('city', city);
+        results.classList.add('button');
         console.log(response);
         return response.json();  
     }) 
     .then(data => {
-        console.log("CURR DAY: ", data);
+        console.log("CURR DAY: ", data); 
         getFiveDays(data.coord)
+      displayCurrentConditions(data);  
 
     }) 
     
     
 };
 
-displayCurrentConditions();
+getCurrentConditions();
 
 // this function is to display the current weather
-function displayCurrentConditions () {
-     ul.classList.add('button');
-     
+function displayCurrentConditions (data) {
+    if (currentWeather.firstChild) {
+        currentWeather.firstChild.remove();
+    }
+    // this is suppose to show the names of the data types, temp, wind speed, and humidity.
+    names.classList.add
+    let h2 = document.createElement('h2');
+    h2.textContent = data.name;
+    currentWeather.append(h2);
+    let li = document.createElement('li');
+    li.textContent = data.main.temp;
+    currentWeather.append(li);
+    let ul = document.createElement('ul');
+    ul.textContent = data.wind.speed;
+    currentWeather.append(ul);
+    let a = document.createElement('a');
+    a.textContent = data.main.humidity;
+    currentWeather.append(a);
+
 }
+    
+
+
 
 // this will get the city from the local storage and display the last searched city 
-ul.innerHTML = localStorage.getItem("city");
-// ul.classList.add('.button');
+results.innerHTML = localStorage.getItem("city");
+
 
 function getFiveDays (coord) {
     var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${weatherApi}`
@@ -52,17 +80,24 @@ function getFiveDays (coord) {
         for(let i = 0; i < data.list.length; i+=8) {
             console.log("FIVE DAYS: ", data.list[i]);
         }
-
+        displayFiveDays(data);
 
     }) 
     
 }
 
+function displayFiveDays (data) {
+   if (fiveDays.firstChild) {
+    fiveDays.firstChild.remove();
+   }
+   
+}
+
 
 btn.addEventListener("click", function () {
     // Obtain city name from the search box
-    var city = $('#search-city').val()
-    getCurrentConditions(city)
+    var city = $('#search-city').val();
+    getCurrentConditions(city);
 });
 
 
